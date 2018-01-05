105bf71d9ae0d6b39c8b0035d8ff3f33cc029c04
Sangmin <sm372181@gmail.com>
2018-01-05 22:57:36 +0900

    [DONE] Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+setup(
+    # introduction
+    name='babbot',
+    version='0.0.1',
+    url='https://github.com/RISMME/bob-bot',
+    description='A package for make kakao-talk school meal bot easily',
+    license='MIT',
+
+    # author
+    author='RISM',
+    author_email='python@istruly.sexy',
+
+    # classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Framework :: Flask',
+
+        'Intended Audience :: Education',
+        'Topic :: Education',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+    ],
+
+    # keyword
+    keywords='School Meal Kakaotalk Chatbot',
+
+    # package
+    package=['bobbot'],
+
+    # requirements
+    install_requires=[
+        'schapi',
+        'flask'
+    ],
+)
